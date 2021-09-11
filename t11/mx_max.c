0a62816b9e26f9e0a31af0819c93166c78b047f5
attapack <75312811+attapack@users.noreply.github.com>
2021-09-11 19:20:40 +0300

    Create mx_max.c

---
+++ t11/mx_max.c
@@ -0,0 +1,11 @@
+int mx_max(int a, int b, int c) { 
+    int max; 
+    if((a > b) && (a > c)) { 
+        max = a; 
+    } else if(b > c) { 
+        max = b; 
+    } else { 
+        max = c; 
+    } 
+    return max;
+}
